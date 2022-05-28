4bf5d0f25ff815c10de3ec241bc7aa921a324871
RoboPhred <robophred@gmail.com>
2022-05-28 11:05:54 -0700

    PressAndHold should hold long press as long as the user is holding the button.

--- src/ButtonCommands/PressAndHoldCommand.cs
+++ src/ButtonCommands/PressAndHoldCommand.cs
@@ -7,7 +7,6 @@ namespace RoboPhredDev.Shipbreaker.SixAxis.ButtonCommands
     class PressAndHoldCommand : IButtonCommand, IUpdatable
     {
         private long? downTick;
-        private bool isTrackingPress;
 
         private long executeTick;
         private ExecutingPress executingPress;
@@ -29,43 +28,47 @@ namespace RoboPhredDev.Shipbreaker.SixAxis.ButtonCommands
         public void Press()
         {
             downTick = DateTime.Now.Ticks;
-            isTrackingPress = true;
         }
 
         public void Release()
         {
-            if (isTrackingPress)
+            // Check if we had a short press.
+            if (downTick.HasValue && downTick.Value + (long)(Duration * TimeSpan.TicksPerSecond) > DateTime.Now.Ticks)
             {
-                isTrackingPress = false;
                 this.ShortPress.Press();
 
+                // Record that we are short pressing so the button logic can run in Update()
                 this.executeTick = DateTime.Now.Ticks;
                 this.executingPress = ExecutingPress.Short;
             }
+
+            // Long presses hold the button down until released by the player.
+            if (executingPress == ExecutingPress.Long)
+            {
+                this.LongPress.Release();
+                executeTick = 0;
+                executingPress = ExecutingPress.None;
+            }
+
+            downTick = null;
         }
 
         public void Update()
         {
-            if (isTrackingPress && downTick + (long)(Duration * TimeSpan.TicksPerSecond) <= DateTime.Now.Ticks)
+            if (downTick.HasValue && downTick.Value + (long)(Duration * TimeSpan.TicksPerSecond) <= DateTime.Now.Ticks)
             {
-                isTrackingPress = false;
                 this.LongPress.Press();
 
                 this.executeTick = DateTime.Now.Ticks;
                 this.executingPress = ExecutingPress.Long;
             }
 
-            if (executingPress != ExecutingPress.None && executeTick + (0.5 * TimeSpan.TicksPerSecond) <= DateTime.Now.Ticks)
+            // If we released on a short press, then we need to hold the short button down for a period of time for the game to register.
+            // Give it a half second.
+            // For long presses, we will keep holding the binding button down until the real button is released.
+            if (executingPress == ExecutingPress.Short && executeTick + (0.5 * TimeSpan.TicksPerSecond) <= DateTime.Now.Ticks)
             {
-                switch (executingPress)
-                {
-                    case ExecutingPress.Short:
-                        this.ShortPress.Release();
-                        break;
-                    case ExecutingPress.Long:
-                        this.LongPress.Release();
-                        break;
-                }
+                this.ShortPress.Release();
                 executeTick = 0;
                 executingPress = ExecutingPress.None;
             }
